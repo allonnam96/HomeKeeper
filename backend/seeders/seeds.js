ac18593a5fe73bd535b44e4181e0b80176e82821
Harrison Liang <liangharrison123@gmail.com>
2023-11-28 15:45:58 -0500

    seeds

--- backend/seeders/seeds.js
+++ backend/seeders/seeds.js
@@ -5,19 +5,17 @@ const { mongoURI: db } = require('../config/keys.js');
 const User = require('../models/User');
 const Contractor = require('../models/Contractor');
 const Category = require('../models/Category');
-// const Review = require('../models/Review')
+const Review = require('../models/Review');
 const bcrypt = require('bcryptjs');
 const { faker } = require('@faker-js/faker');
 
 const NUM_SEED_USERS = 10;
-const NUM_SEED_CONTRACTORS = 30;
+const NUM_SEED_CONTRACTORS = 100;
 
 const users = [];
-const contractors = [];
 
 const seedCategories = async () => {
   const categories = ['Plumber', 'Painter', 'Electrician', 'Home Remodeler', 'Lawyer'];
-
   return await Category.insertMany(categories.map(category => ({ name: category })));
 };
 
@@ -25,16 +23,25 @@ const generateFakeContractor = (categoryObjectIds) => {
   const firstName = faker.person.firstName();
   const lastName = faker.person.lastName();
   const phoneFormats = ['###-###-####', '(###) ###-####', '1-###-###-####'];
-  // const randomPhoneFormat = phoneFormats[Math.floor(Math.random() * phoneFormats.length)];
   const phoneNum = formatPhoneNumber();
   const randomCategoryObjectId = categoryObjectIds[Math.floor(Math.random() * categoryObjectIds.length)]._id;
-  
+
+  const imageUrl = 'https://source.unsplash.com/400x400/?headshot%20professional';
+
+  const genericPhrases = [
+    'Experienced professional in the field',
+    'Passionate about delivering high-quality service',
+    'Committed to customer satisfaction',
+    'Skilled and reliable contractor',
+    'Dedicated to excellence in every project',
+  ];
+
   return new Contractor({
     name: `${firstName} ${lastName}`,
     title: faker.name.jobTitle(),
-    bio: faker.lorem.paragraph(),
+    bio: genericPhrases[Math.floor(Math.random() * genericPhrases.length)],
     address: faker.address.streetAddress(),
-    photoUrl: faker.image.imageUrl(),
+    photoUrl: imageUrl,
     phoneNum: phoneNum,
     email: faker.internet.email(firstName),
     category: randomCategoryObjectId,
@@ -45,7 +52,6 @@ const formatPhoneNumber = () => {
   const phoneFormats = ['###-###-####', '(###) ###-####', '1-###-###-####'];
   const randomPhoneFormat = phoneFormats[Math.floor(Math.random() * phoneFormats.length)];
 
-  // Replace '#' with random digits
   return randomPhoneFormat.replace(/#/g, () => Math.floor(Math.random() * 10));
 };
 
@@ -74,10 +80,11 @@ const seedData = async () => {
     );
   }
 
+  const contractors = [];
   for (let i = 0; i < NUM_SEED_CONTRACTORS; i++) {
     const newContractor = generateFakeContractor(categoryObjectIds);
     contractors.push(newContractor);
-  };
+  }
 
   try {
     console.log("Resetting db and seeding users and contractors...");
@@ -85,7 +92,21 @@ const seedData = async () => {
     await User.deleteMany({});
     await Contractor.deleteMany({});
     await User.insertMany(users);
-    await Contractor.insertMany(contractors);
+    const insertedContractors = await Contractor.insertMany(contractors);
+
+    // Generate and seed reviews
+    const reviews = [];
+    for (let i = 0; i < 200; i++) {
+      const contractor = categoryObjectIds[Math.floor(Math.random() * categoryObjectIds.length)];
+      reviews.push({
+        reviewStar: faker.datatype.number({ min: 1, max: 5 }),
+        reviewSummary: faker.lorem.sentence(),
+        contractor: contractor._id,
+      });
+    }
+
+    await Review.deleteMany({});
+    await Review.insertMany(reviews);
 
     console.log("Seeding completed!");
   } catch (err) {
