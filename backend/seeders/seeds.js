51583b4dba96cee2e8a7cabb607587544e276ca3
Harrison Liang <liangharrison123@gmail.com>
2023-11-28 09:52:36 -0500

    seeds

--- backend/seeders/seeds.js
+++ backend/seeders/seeds.js
@@ -1,3 +1,78 @@
+// require('dotenv').config();
+// const mongoose = require('mongoose');
+// const { mongoURI: db } = require('../config/keys.js');
+// const Contractor = require('../models/Contractor');
+// const { check } = require('express-validator');
+// const handleValidationErrors = require('./handleValidationErrors');
+// const faker = require('faker');
+
+// const NUM_SEED_CONTRACTORS = 10;
+
+// const generateFakeContractor = () => {
+//   return {
+//     name: faker.name.findName(),
+//     title: faker.name.jobTitle(),
+//     reviewStar: faker.random.number({ min: 1, max: 5 }),
+//     bio: faker.lorem.paragraph(),
+//     address: faker.address.streetAddress(),
+//     photoUrl: faker.image.imageUrl(),
+//     category: mongoose.Types.ObjectId() // Assuming this generates a new ObjectId
+//   };
+// };
+
+// const contractors = [];
+
+// for (let i = 0; i < NUM_SEED_CONTRACTORS; i++) {
+//   const newContractor = generateFakeContractor();
+//   contractors.push(newContractor);
+// }
+
+// // Validate contractors before insertion
+// const validateContractorInput = [
+//   check('name').exists({ checkFalsy: true }).withMessage('Name is required'),
+//   check('title').exists({ checkFalsy: true }).withMessage('Title is required'),
+//   check('reviewStar')
+//     .isInt({ min: 1, max: 5 })
+//     .withMessage('Review star must be between 1 and 5'),
+//   check('bio').exists({ checkFalsy: true }).withMessage('Bio is required'),
+//   check('address').exists({ checkFalsy: true }).withMessage('Address is required'),
+//   check('photoUrl').exists({ checkFalsy: true }).withMessage('Photo URL is required'),
+//   check('category')
+//     .exists({ checkFalsy: true })
+//     .withMessage('Category is required')
+//     .isMongoId()
+//     .withMessage('Category should be a valid ID'),
+//   handleValidationErrors
+// ];
+
+// // Connect to the database and insert contractors
+// const insertContractors = async () => {
+//   try {
+//     console.log('Resetting db and seeding contractors...');
+
+//     await Contractor.deleteMany({});
+//     await Promise.all(contractors.map(contractor => Contractor.create(contractor)));
+
+//     console.log('Seeding completed!');
+//     mongoose.disconnect();
+//   } catch (err) {
+//     console.error(err.stack);
+//     process.exit(1);
+//   }
+// };
+
+// // Connect to the database
+// mongoose
+//   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => {
+//     console.log('Connected to MongoDB successfully');
+//     insertContractors();
+//   })
+//   .catch((err) => {
+//     console.error(err.stack);
+//     process.exit(1);
+//   });
+
 require('dotenv').config();
 
 const mongoose = require("mongoose");
@@ -9,42 +84,57 @@ const { faker } = require('@faker-js/faker');
 // Create your seeds (users and tweets)
 const NUM_SEED_USERS = 10;
 
-const users = [];
+const generateFakeContractor = () => {
+  return {
+    name: faker.name.findName(),
+    title: faker.name.jobTitle(),
+    reviewStar: faker.random.number({ min: 1, max: 5 }),
+    bio: faker.lorem.paragraph(),
+    address: faker.address.streetAddress(),
+    photoUrl: faker.image.imageUrl(),
+    category: mongoose.Types.ObjectId() // Assuming this generates a new ObjectId
+  };
+};
 
-users.push(
-  new User({
-    username: 'demo-user',
-    email: 'demo-user@appacademy.io',
-    hashedPassword: bcrypt.hashSync('starwars', 10)
-  })
-);
+const contractors = [];
 
-for (let i = 1; i < NUM_SEED_USERS; i++) {
-  const firstName = faker.name.firstName();
-  const lastName = faker.name.lastName();
-  users.push(
-    new User({
-      name: faker.internet.Name(firstName, lastName),
-      email: faker.internet.email(firstName, lastName),
-      hashedPassword: bcrypt.hashSync(faker.internet.password(), 10)
-    })
-  );
+for (let i = 0; i < NUM_SEED_CONTRACTORS; i++) {
+  const newContractor = generateFakeContractor();
+  contractors.push(newContractor);
 }
 
-// Connect to the database and insert your seeds
-const insertSeeds = () => {
-  console.log("Resetting db and seeding users and tweets...");
+// Validate contractors before insertion
+const validateContractorInput = [
+  check('name').exists({ checkFalsy: true }).withMessage('Name is required'),
+  check('title').exists({ checkFalsy: true }).withMessage('Title is required'),
+  check('reviewStar')
+    .isInt({ min: 1, max: 5 })
+    .withMessage('Review star must be between 1 and 5'),
+  check('bio').exists({ checkFalsy: true }).withMessage('Bio is required'),
+  check('address').exists({ checkFalsy: true }).withMessage('Address is required'),
+  check('photoUrl').exists({ checkFalsy: true }).withMessage('Photo URL is required'),
+  check('category')
+    .exists({ checkFalsy: true })
+    .withMessage('Category is required')
+    .isMongoId()
+    .withMessage('Category should be a valid ID'),
+  handleValidationErrors
+];
 
-  User.collection.drop()
-    .then(() => User.insertMany(users))
-    .then(() => {
-      console.log("Done!");
-      mongoose.disconnect();
-    })
-    .catch(err => {
-      console.error(err.stack);
-      process.exit(1);
-    });
+// Connect to the database and insert contractors
+const insertContractors = async () => {
+  try {
+    console.log('Resetting db and seeding contractors...');
+
+    await Contractor.deleteMany({});
+    await Promise.all(contractors.map(contractor => Contractor.create(contractor)));
+
+    console.log('Seeding completed!');
+    mongoose.disconnect();
+  } catch (err) {
+    console.error(err.stack);
+    process.exit(1);
+  }
 };
 
 // Connect to the database
