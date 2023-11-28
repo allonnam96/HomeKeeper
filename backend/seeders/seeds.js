2a50d5fc6262f9526e8b5bc06c2ddf3485f2b63e
Harrison Liang <liangharrison123@gmail.com>
2023-11-28 11:10:36 -0500

    added more contractors

--- backend/seeders/seeds.js
+++ backend/seeders/seeds.js
@@ -1,91 +1,3 @@
-// require('dotenv').config();
-
-// const mongoose = require('mongoose');
-// const { mongoURI: db } = require('../config/keys.js');
-// const User = require('../models/User');
-// // const Contractor = require('../models/Contractor');
-// const bcrypt = require('bcryptjs');
-// const { faker } = require('@faker-js/faker');
-
-// const NUM_SEED_USERS = 10;
-// // const NUM_SEED_CONTRACTORS = 10;
-
-// const users = [];
-
-// users.push(
-//   new User({
-//     name: 'Demo User',
-//     email: 'demo-user@appacademy.io',
-//     hashedPassword: bcrypt.hashSync('starwars', 10),
-//     birthday: faker.date.past(30), // Adjust the age as needed
-//   })
-// );
-
-// for (let i = 1; i < NUM_SEED_USERS; i++) {
-//   const firstName = faker.person.firstName();
-//   const lastName = faker.person.lastName();
-//   users.push(
-//     new User({
-//       name: `${firstName} ${lastName}`,
-//       email: faker.internet.email({ firstName, lastName }),
-//       hashedPassword: bcrypt.hashSync(faker.internet.password(), 10),
-//       birthday: faker.date.past(30), // Adjust the age as needed
-//     })
-//   );
-// }
-
-// // const contractors = [];
-// // const categories = ['Plumber', 'Painter', 'Electrician', 'Home Remodeler', 'Lawyer'];
-
-// // const generateFakeContractor = () => {
-// //   const randomCategory = categories[Math.floor(Math.random() * categories.length)];
-// //   return new Contractor({
-// //     person: faker.name.firstName(),
-// //     title: faker.name.jobTitle(),
-// //     reviewStar: faker.datatype.number({ min: 1, max: 5 }),
-// //     bio: faker.lorem.paragraph(),
-// //     address: faker.address.streetAddress(),
-// //     photoUrl: faker.image.imageUrl(),
-// //     category: randomCategory,
-// //   });
-// // };
-
-// // for (let i = 0; i < NUM_SEED_CONTRACTORS; i++) {
-// //   const newContractor = generateFakeContractor();
-// //   contractors.push(newContractor);
-// // }
-
-// // Connect to the database and insert your seeds
-// const insertSeeds = () => {
-//   console.log("Resetting db and seeding users and contractors...");
-
-//   User.deleteMany({})
-//     // .then(() => Contractor.deleteMany({}))
-//     .then(() => User.insertMany(users))
-//     // .then(() => Contractor.insertMany(contractors))
-//     .then(() => {
-//       console.log("Seeding completed!");
-//       mongoose.disconnect();
-//     })
-//     .catch(err => {
-//       console.error(err.stack);
-//       process.exit(1);
-//     });
-// };
-
-// // Connect to the database
-// mongoose
-//   .connect(db, { useNewUrlParser: true })
-//   .then(() => {
-//     console.log('Connected to MongoDB successfully');
-//     insertSeeds();
-//   })
-//   .catch(err => {
-//     console.error(err.stack);
-//     process.exit(1);
-//   });
-
-
 require('dotenv').config();
 
 const mongoose = require('mongoose');
@@ -97,7 +9,7 @@ const bcrypt = require('bcryptjs');
 const { faker } = require('@faker-js/faker');
 
 const NUM_SEED_USERS = 10;
-const NUM_SEED_CONTRACTORS = 10;
+const NUM_SEED_CONTRACTORS = 30;
 
 const users = [];
 const contractors = [];
@@ -108,32 +20,9 @@ const seedCategories = async () => {
   return await Category.insertMany(categories.map(category => ({ name: category })));
 };
 
-// users.push(
-//   new User({
-//     name: 'Demo User',
-//     email: 'demo-user@appacademy.io',
-//     hashedPassword: bcrypt.hashSync('starwars', 10),
-//     birthday: faker.date.past(30),
-//   })
-// );
-
-// for (let i = 1; i < NUM_SEED_USERS; i++) {
-//   const firstName = faker.person.firstName();
-//   const lastName = faker.person.lastName();
-//   users.push(
-//     new User({
-//       name: `${firstName} ${lastName}`,
-//       email: faker.internet.email({ firstName, lastName }),
-//       hashedPassword: bcrypt.hashSync(faker.internet.password(), 10),
-//       birthday: faker.date.past(30),
-//     })
-//   );
-// }
-
 const generateFakeContractor = (categoryObjectIds) => {
   const firstName = faker.person.firstName();
   const lastName = faker.person.lastName();
-  // const randomCategory = categories[Math.floor(Math.random() * categories.length)];
   const randomCategoryObjectId = categoryObjectIds[Math.floor(Math.random() * categoryObjectIds.length)]._id;
   return new Contractor({
     name: `${firstName} ${lastName}`,
@@ -142,38 +31,13 @@ const generateFakeContractor = (categoryObjectIds) => {
     bio: faker.lorem.paragraph(),
     address: faker.address.streetAddress(),
     photoUrl: faker.image.imageUrl(),
-    // category: randomCategory,
     category: randomCategoryObjectId,
   });
 };
 
-// for (let i = 0; i < NUM_SEED_CONTRACTORS; i++) {
-//   const newContractor = generateFakeContractor();
-//   contractors.push(newContractor);
-// }
-
-// const insertSeeds = () => {
-//   console.log("Resetting db and seeding users and contractors...");
-
-//   User.deleteMany({})
-//     .then(() => Contractor.deleteMany({}))
-//     .then(() => User.insertMany(users))
-//     .then(() => Contractor.insertMany(contractors))
-//     .then(() => {
-//       console.log("Seeding completed!");
-//       mongoose.disconnect();
-//     })
-//     .catch(err => {
-//       console.error(err.stack);
-//       process.exit(1);
-//     });
-// };
-
 const seedData = async () => {
-  // Seed categories
   const categoryObjectIds = await seedCategories();
 
-  // Seed users
   users.push(
     new User({
       name: 'Demo User',
@@ -196,13 +60,11 @@ const seedData = async () => {
     );
   }
 
-  // Seed contractors
   for (let i = 0; i < NUM_SEED_CONTRACTORS; i++) {
     const newContractor = generateFakeContractor(categoryObjectIds);
     contractors.push(newContractor);
   };
 
-  // Connect to the database and insert your seeds
   try {
     console.log("Resetting db and seeding users and contractors...");
 
