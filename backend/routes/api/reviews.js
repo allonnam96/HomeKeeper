e2eddf135b4bad5efa4ebf8a72ebb35f03fb4cda
Allon Nam <allon.nam96@gmail.com>
2023-11-29 14:55:54 -0500

    reviews done

---
+++ backend/routes/api/reviews.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const Category = mongoose.model('Category');
+const Review = mongoose.model('Review');
+const { requireUser } = require('../../config/passport');
+const Contractor = require('../../models/Contractor');
+
+router.get('/', async (req, res) => {
+    try{
+        const reviews = await Review.find()
+        .populate('contractor', '_id name');
+        return res.json(reviews)
+    }
+    catch(err) {
+        return res.json([]);
+    }
+});
+
+router.get('/:contractorId/reviews', async (req, res, next) => {
+    try {
+        const reviews = await Review.find({ contractor: req.params.contractorId})
+        .populate('contractor', '_id ');
+
+        return res.json(reviews);
+    }
+    catch (err) {
+        const error = new Error('Error retrieving reviews for this contractor');
+        error.statusCode = 500;
+        return next(error);
+    }
+});
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const review = await Review.findById(req.params.id)
+            .populate('contractor', '_id name');
+
+        return res.json(review);
+    }
+    catch (err) {
+        const error = new Error('Contractor not found');
+        error.statusCode = 404;
+        error.errors = { message: "No Contractor found with that id" }
+        return next(error);
+    }
+});
+
+module.exports = router;
