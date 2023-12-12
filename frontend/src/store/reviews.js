2841d65976b7050285d420bcf99faa7ead2971a3
omar-t-ahmed <omartahmed@gmail.com>
2023-12-12 11:47:22 -0500

    add reviews reducer

--- frontend/src/store/reviews.js
+++ frontend/src/store/reviews.js
@@ -2,32 +2,81 @@ import jwtFetch from './jwt';
 
 const RECEIVE_REVIEWS = "reviews/RECEIVE_REVIEWS";
 const RECEIVE_REVIEW = "reviews/RECEIVE_REVIEW";
+const CREATE_REVIEW = "reviews/CREATE_REVIEW";
+const UPDATE_REVIEW = "reviews/UPDATE_REVIEW";
+const DELETE_REVIEW = "reviews/DELETE_REVIEW";
 
 const receiveReviews = reviews => ({
     type: RECEIVE_REVIEWS,
     reviews
-});
+})
 
 const receiveReview = review => ({
     type: RECEIVE_REVIEW,
     review
-});
+})
+
+const deleteReview = reviewId => ({
+    type: DELETE_REVIEW,
+    reviewId
+})
+
 
 export const fetchReviews = () => async dispatch => {
-    const res = await jwtFetch ('/api/reviews');
+    const res = await jwtFetch ('/api/reviews')
     if (res.ok) {
-        const reviews = await res.json();
-        dispatch(receiveReviews(reviews));
+        const reviews = await res.json()
+        dispatch(receiveReviews(reviews))
     }
-};
+}
 
 export const fetchReview = (reviewId) => async dispatch => {
-    const res = await jwtFetch (`/api/reviews/${reviewId}`);
+    const res = await jwtFetch (`/api/reviews/${reviewId}`)
     if (res.ok) {
-        const review = await res.json();
-        dispatch(receiveReview(review));
+        const review = await res.json()
+        dispatch(receiveReview(review))
     }
-};
+}
+
+export const addReview = (review) => async dispatch => {
+    const res = await jwtFetch('/api/reviews/new', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(review),
+    })
+
+    if (res.ok) {
+        const review = await res.json()
+        dispatch(receiveReview(review))
+    }
+}
+
+export const updateReview = (updatedReview) => async dispatch => {
+    const res = await jwtFetch(`/api/reviews/${updatedReview.id}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updateFields),
+    })
+
+    if (res.ok) {
+        const updatedReview = await res.json()
+        dispatch(receiveReview(updatedReview))
+    }
+}
+
+export const removeReview = (reviewId) => async dispatch => {
+    const res = await jwtFetch(`/api/reviews/${reviewId}`, {
+        method: 'DELETE',
+    })
+
+    if (res.ok) {
+        dispatch(deleteReview(reviewId))
+    }
+}
 
 export const getReviews = (state, contractorId) => {
     let reviews = []
@@ -57,11 +106,20 @@ const reviewsReducer = (state = { }, action) => {
     const newState = {...state}
     switch(action.type) {
         case RECEIVE_REVIEWS:
-            return {...action.reviews};
+            let reviews = {}
+
+            action.reviews.forEach((review) => {
+                reviews[review._id] = review
+            })
+            return reviews
+            
         case RECEIVE_REVIEW:
             return {...newState, [action.review._id] : action.review}
+        case DELETE_REVIEW:
+            delete newState[action.reviewId]
+            return newState
         default:
-            return state;
+            return state
     }
 }
 
