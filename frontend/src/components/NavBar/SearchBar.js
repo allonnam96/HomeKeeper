8d36d6bebb5f78d291dfe4d12aef7e6f7f175a39
Harrison Liang <liangharrison123@gmail.com>
2023-12-12 17:27:38 -0500

    dropdown disappear after click

--- frontend/src/components/NavBar/SearchBar.js
+++ frontend/src/components/NavBar/SearchBar.js
@@ -1,35 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { searchContractors } from '../../store/contractors';
-// import './NavBar.css';
-
-// const SearchBar = () => {
-//   const dispatch = useDispatch();
-//   const [searchQuery, setSearchQuery] = useState('');
-//   const contractors = useSelector((state) => state.contractors);
-
-//   useEffect(() => {
-//     // Dispatch searchContractors when the searchQuery changes
-//     dispatch(searchContractors(searchQuery));
-//   }, [dispatch, searchQuery]);
-
-//   const handleInputChange = (e) => {
-//     setSearchQuery(e.target.value);
-//   };
-
-//   return (
-//     <input
-//       type="text"
-//       placeholder="Search HomeKeeper"
-//       id="search-bar"
-//       value={searchQuery}
-//       onChange={handleInputChange}
-//     />
-//   );
-// };
-
-// export default SearchBar;
-
 import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { searchContractors, clearSearchResults } from '../../store/contractors';
@@ -65,26 +33,6 @@ const SearchBar = () => {
   };
 
   return (
-    // <div className="search-bar-container">
-    //   <input
-    //     type="text"
-    //     placeholder="Search HomeKeeper"
-    //     id="search-bar"
-    //     value={searchQuery}
-    //     onChange={handleInputChange}
-    //   />
-    //   {searchResults.length > 0 && (
-    //     <div className="search-results-dropdown">
-    //       {searchResults.map((result) => (
-    //         // Render each search result as needed
-    //         <div key={result._id} className="search-result-item">
-    //           <img src={result.photoUrl} />
-    //           {result.name} {/* Adjust this based on your contractor object structure */}
-    //         </div>
-    //       ))}
-    //     </div>
-    //   )}
-    // </div>
     <div className="search-bar-container">
       <input
         type="text"
@@ -97,7 +45,12 @@ const SearchBar = () => {
         <div className="search-results-dropdown">
           {searchResults.map((result) => (
             // Render each search result as needed
-            <div key={result._id} className="search-result-item" onClick={() => { history.push(`/contractors/${result._id}`) }}>
+            <div key={result._id} className="search-result-item" onClick={() => { 
+              history.push(`/contractors/${result._id}`) 
+              setSearchQuery('');
+              setSearchResults([]);
+              dispatch(clearSearchResults());
+            }}>
               <img src={result.photoUrl} alt={result.name} />
               <span>{result.name}</span>
             </div>
